1ab2f18fd54473ab5af438007bebaeecb1c8e969
shdx8 <elyasagus.29@gmail.com>
2021-06-18 09:44:48 +0700

    update

--- D01/blockofCode4.py
+++ D01/blockofCode4.py
@@ -1,3 +1,3 @@
 if 5 > 2:
-    print ("Selamat belajar!")
-    print ("Semangat Yah!")
+ print ("Selamat belajar!")
+ print ("Semangat Yah!")
---
+++ D01/dataType-type.py
@@ -0,0 +1,4 @@
+x = 5
+print (x, "tipenya adalah ", type(x))
+x = 2.0
+print (x, "tipenya adalah ", type(x))
